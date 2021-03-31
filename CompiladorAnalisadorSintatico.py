49fb60ed5d61752deb8d6ec89217f841f2954f97
Maurício Bernardo de Souza <50925332+MauricioSuporte@users.noreply.github.com>
2021-03-30 20:38:27 -0400

    Corrige bug funções Sintáticas.
    PS: Comentários ainda não funcionam.

--- CompiladorAnalisadorSintatico.py
+++ CompiladorAnalisadorSintatico.py
@@ -92,11 +92,11 @@ def dc_v(ch, pos):
             ch, pos = variaveis(ch, pos)
             if ch == ":":
                 ch, pos = proxsimb(ch, pos)
-                if ch == "begin" or ch == ";" or ch == ")":
+                if ch == "real" or ch == "integer":
                     ch, pos = tipo_var(ch, pos)
                     return (ch, pos)
                 else:
-                    print("Erro, esperado : e encontrado %s no %dº token" %(ch, pos+1))
+                    print("Erro, esperado real ou integer e encontrado %s no %dº token" %(ch, pos+1))
                     exit()  
             else:
                 print("Erro, esperado : e encontrado %s no %dº token" %(ch, pos+1))
@@ -196,16 +196,16 @@ def lista_par(ch, pos):
         ch, pos = variaveis(ch, pos)
         if ch == ":":
             ch, pos = proxsimb(ch, pos)
-            if ch == "begin" or ch == ";" or ch == ")":
+            if ch == "real" or ch == "integer":
                 ch, pos = tipo_var(ch, pos)
-                if ch == "," or ch == ")" or ch == ":":
-                    ch, pos = mais_var(ch, pos)
+                if ch == ";" or ch == ")":
+                    ch, pos = mais_par(ch, pos)
                     return (ch, pos)
                 else:
-                    print("Erro, esperado , ou ) ou : e encontrado %s no %dº token" %(ch, pos+1))
+                    print("Erro, esperado ; ou ) e encontrado %s no %dº token" %(ch, pos+1))
                     exit() 
             else:
-                print("Erro, esperado begin ou ; ou ) e encontrado %s no %dº token" %(ch, pos+1))
+                print("Erro, esperado real ou integer e encontrado %s no %dº token" %(ch, pos+1))
                 exit() 
         else:
             print("Erro, esperado : e encontrado %s no %dº token" %(ch, pos+1))
@@ -219,6 +219,7 @@ def mais_par(ch, pos):
         ch, pos = proxsimb(ch, pos)
         if isIdent(ch):
             ch, pos = lista_par(ch, pos)
+            return (ch, pos)
         else:
             print("Erro, esperado identificador e encontrado %s no %dº token" %(ch, pos+1))
             exit() 
@@ -358,7 +359,7 @@ def mais_comandos(ch, pos):
     if ch == ";":
         ch, pos = proxsimb(ch, pos)
         if isIdent(ch) or ch == "read" or ch == "write" or ch =="while" or ch == "if":
-            ch, pos = comando(ch, pos)
+            ch, pos = comandos(ch, pos)
             return (ch, pos)
         else:
             print("Erro, esperado identificador ou read ou write ou while ou if e encontrado %s no %dº token" %(ch, pos+1))
@@ -373,6 +374,7 @@ def comando(ch, pos):
     if ch == "read":
         ch, pos = proxsimb(ch, pos)
         if ch == "(":
+            ch, pos = proxsimb(ch, pos)
             if isIdent(ch):
                 ch, pos = variaveis(ch, pos)
                 if ch == ")":
@@ -390,6 +392,7 @@ def comando(ch, pos):
     elif ch == "write":
         ch, pos = proxsimb(ch, pos)
         if ch == "(":
+            ch, pos = proxsimb(ch, pos)
             if isIdent(ch):
                 ch, pos = variaveis(ch, pos)
                 if ch == ")":
@@ -602,176 +605,6 @@ def fator(ch, pos):
         print("Erro, esperado identificador ou numero_real ou numero_inteiro ou ( e encontrado %s no %dº token" %(ch, pos+1))
         exit() 
 
-def Z(ch, pos):
-    if ch == "var":
-        ch, pos = I(ch, pos)
-        ch, pos = S(ch, pos)
-        print("Cadeia sintaticamente correta.")
-    else:
-        print("Erro, esperado var e encontrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-def I(ch, pos):
-    if ch == "var":
-        ch, pos = proxsimb(ch, pos)
-        ch, pos = D(ch, pos)
-        return (ch, pos)
-    else:
-        print("Erro, esperado var e encontrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-def D(ch, pos):
-    if isIdent(ch):
-        ch, pos = L(ch, pos)
-        if ch == ":":
-            ch, pos = proxsimb(ch, pos)
-            ch, pos = K(ch, pos)
-            ch, pos = O(ch, pos)
-            return (ch, pos)
-        else:
-            print("Erro, esperado : e encontrado %s no %dº token" %(ch, pos+1))
-            exit()
-    elif ch == "if":
-        return (ch, pos)
-    else:
-        print("Erro, esperado identificador ou if e encontrado %s no %dº token" % (ch, pos + 1))
-        exit()
-
-def L(ch, pos):
-    if isIdent(ch):
-        addTabSimb(ch)
-        ch, pos = proxsimb(ch, pos)
-        ch, pos = X(ch, pos)
-        return(ch, pos)
-    else:
-        print("Erro, esperado identificador e encontrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-def X(ch, pos):
-    if ch == ",":
-        ch, pos = proxsimb(ch, pos)
-        ch, pos = L(ch, pos)
-        return(ch, pos)
-    elif ch == ":":
-        return (ch, pos)
-    else:
-        print("Erro, esperado , ou : e encontrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-def K(ch, pos):
-    if ch == "integer":
-        addTipo(ch, ch)
-        ch, pos = proxsimb(ch, pos)
-        return (ch, pos)
-    elif ch == "real":
-        addTipo(ch, ch)
-        ch, pos = proxsimb(ch, pos)
-        return (ch, pos)
-    else:
-        print("Erro, esperado integer ou real e econtrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-def O(ch, pos):
-    if ch == ";":
-        ch, pos = proxsimb(ch, pos)
-        ch, pos = D(ch, pos)
-        return (ch, pos)
-    elif isIdent(ch):
-        return (ch, pos)
-    elif ch == "if":
-        return (ch, pos)
-    else:
-        print("Erro, esperado ; ou identificador ou if e econtrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-def S(ch, pos):
-    global cod3End, linhaCod
-    if isIdent(ch):
-        if not existe(ch):
-            print("Identificador %s na posicao %sº nao declarado" %(ch, str(pos)))
-            exit()
-        addListaVerificacao(ch)
-        EEsq = ch
-        ch, pos = proxsimb(ch, pos)
-        if ch == ":=":
-            ch, pos = proxsimb(ch, pos)
-            ch, pos, EDir = E(ch, pos)
-            linha = "%d: [:= %s %s %s]" % (linhaCod, EEsq, EDir, "-")
-            cod3End.insert(linhaCod, linha)
-            linhaCod = linhaCod + 1
-            return (ch, pos)
-        else:
-            print("Erro, esperado := e econtrado %s no %dº token" %(ch, pos+1))
-            exit()
-    elif ch == "if":
-        ch, pos = proxsimb(ch, pos)
-        ch, pos, EDir = E(ch, pos)
-        if ch == "then":
-            S1Quad = linhaCod - 1
-            linhaCod = linhaCod + 1
-            ch, pos = proxsimb(ch, pos)
-            ch, pos = S(ch, pos)
-            linha = "%d: [JF %s %s %s]" % (S1Quad + 1, EDir, '<T>', "-")
-            cod3End.insert(S1Quad, linha)
-            linhaCod = linhaCod + 1
-            return (ch, pos)
-        else:
-            print("Erro, esperado then e econtrado %s no %dº token" %(ch, pos+1))
-            exit()
-    else:
-        print("Erro, esperado identificador ou if e econtrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-def E(ch, pos):
-    if isIdent(ch):
-        ch, pos, REsq = T(ch, pos)
-        ch, pos, EDir = R(ch, pos, REsq)
-        return (ch, pos, EDir)
-    else:
-        print("Erro, esperado identificador e econtrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-def R(ch, pos, REsq):
-    if ch == "+":
-        ch, pos = proxsimb(ch, pos)
-        ch, pos, R1Esq = T(ch, pos)
-        ch, pos, R1Dir = R(ch, pos, R1Esq)
-        RDir = geraTemp(REsq)
-        global cod3End, linhaCod
-        linha = "%d: [+ %s %s %s]" %(linhaCod, REsq, R1Dir, RDir)
-        cod3End.insert(linhaCod, linha)
-        linhaCod = linhaCod + 1
-        return (ch, pos, RDir)
-    elif ch == "then":
-        global verificacoes
-        if (not verificaTipo(verificacoes)):
-            exit()
-        verificacoes = []
-        return (ch, pos, REsq)
-    elif ch == "#":
-        if (not verificaTipo(verificacoes)):
-            exit()
-        verificacoes = []
-        return (ch, pos, REsq)
-    else:
-        print("Erro, esperado + ou then e econtrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-
-def T(ch, pos):
-    if isIdent(ch):
-        if not existe(ch):
-            print("Identificador %s na posicao %sº nao declarado" %(ch, str(pos)))
-            exit()
-        addListaVerificacao(ch)
-        TDir = ch
-        ch, pos = proxsimb(ch, pos)
-        return (ch, pos, TDir)
-    else:
-        print("Erro, esperado identificador e econtrado %s no %dº token" %(ch, pos+1))
-        exit()
-
-
 def proxsimb(ch, pos):
     if pos < len(tokens)-1:
         return (tokens[pos + 1], pos + 1)
@@ -779,23 +612,26 @@ def proxsimb(ch, pos):
         return ("#", pos)
 
 def isIdent(ch):
-    if ch == "var" or ch == "integer" or ch == "real" or ch == "if" or ch == "then":
-        resultado = False
+    if ch == "var" or ch == "integer" or ch == "real" or ch == "if" or ch == "then" or ch == "do" or ch == "(" or ch == ")" or ch == "*" or ch == "/" or ch == "+" or ch == "-" or ch == "<>" or ch == ">=" or ch == "<=" or ch == ">" or ch == "<" or ch == "=" or ch == "$" or ch == "while" or ch == "write" or ch == "real" or ch == ";" or ch == "else" or ch == "begin" or ch == "end" or ch == ":" or ch == ",":
+        return False
     elif (ch[0] >= "a" and ch[0] <= "z") or (ch[0] >= "A" and ch[0] <= "Z"):
-        resultado = True
+        return True
     else:
-        resultado = False
-    return resultado
+        return False
 
 def isInt(ch):
-    if "." in ch:
+    try: 
+        int(ch)
+        return True
+    except ValueError:
         return False
-    return True
 
 def isReal(ch):
-    if "." in ch:
+    try: 
+        float(ch)
         return True
-    return False
+    except ValueError:
+        return False
 
 def addTabSimb(ch):
     global posFinal, tabSimb
@@ -848,16 +684,3 @@ def geraTemp(ch):
 #Main
 programa(tokens[0], 0)
 arquivo.close()
-
-# print("\n=-=-=-=-=-=-=-=-=-=-=-=-=TABELA DE SIMBOLOS=-=-=-=-=-=-=-=-=-=-=-=-=")
-# for i in range(len(tabSimb)):
-#     print(tabSimb[i])
-
-# print("\n\n=-=CODIGO INTERMEDIARIO=-=")
-# cod3End = sorted(cod3End)
-# for i in range(len(cod3End)):
-#     if cod3End[i][4] == 'J':
-#         cod3End[i] = cod3End[i].replace('<T>', str(len(cod3End) + 1))
-# for i in cod3End:
-#     print(f'{i}')
-# print(str(len(cod3End) + 1) + ": [...]")
