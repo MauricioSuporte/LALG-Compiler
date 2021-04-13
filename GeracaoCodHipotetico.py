a3c0e16a907874e02b3e9e0ded781846132531f3
Maurício Bernardo de Souza <50925332+MauricioSuporte@users.noreply.github.com>
2021-04-13 01:47:30 -0400

    Interpreta entrada e retorna resultado

--- GeracaoCodHipotetico.py
+++ GeracaoCodHipotetico.py
@@ -1,5 +1,7 @@
-arquivo = open("codigo.txt", "r")
-pilhadeDados = []
+C = [] #Lista da área de código
+i = 0 #Posição da lista da área de código
+D = [] #Pilha da área de dados
+s = -1 #Posição da pila da área de dados
 nLinha = -1
 comando = ""
 valor = ""
@@ -7,136 +9,244 @@ valor = ""
 #Funções Cod Hipotetico
 #carrega constante k no topo da pilha D
 def CRCT(k):
-    return
+    global D, s
+    s += 1
+    D.append(0)
+    D[s] = k
 
 #carrega valor de endereço n no topo da pilha D
 def CRVL(n):
-    return
+    global D, s
+    s = s+1
+    D.append(D[n])
 
 #soma o elemento antecessor com o topo da pilha
 def SOMA():
-    return
+    global D, s
+    D[s-1] = D[s-1] + D[s]
+    s = s-1
+    D.pop()
 
 #subtrai o antecessor pelo elemento do topo
 def SUBT():
-    return
+    global D, s
+    D[s-1] = D[s-1] - D[s]
+    s = s-1
+    D.pop()
 
 #multiplica elemento antecessor pelo elemento do topo
 def MULT():
-    return
+    global D, s
+    D[s-1] = D[s-1] * D[s]
+    s = s-1
+    D.pop()
 
 #divide o elemento antecessor pelo elemento do topo
 def DIVI():
-    return
+    global D, s
+    if D[s] == 0:
+        print("Impossivel dividir por 0")
+        exit()
+    D[s-1] = D[s-1] / D[s]
+    s = s-1
+    D.pop()
 
 #inverte sinal do topo
 def INVE():
-    return
-
-#conjunção de valores lógicos. F=0; V=1
-def CONJ():
-    return
-
-#disjunção de valores lógicos
-def DISJ():
-    return
-
-#negação lógica
-def NEGA():
-    return
+    global D, s
+    D[s] = - D[s]
 
 #comparação de menor entre o antecessor e o topo
 def CPME():
-    return
+    global D, s
+    if D[s-1] < D[s]:
+        D[s-1] = 1
+    else:
+        D[s-1] = 0
+    s = s-1
+    D.pop()
 
 #comparação de maior
 def CPMA():
-    return
+    global D, s
+    if D[s-1] > D[s]:
+        D[s-1] = 1
+    else:
+        D[s-1] = 0
+    s = s-1
+    D.pop()
 
 #comparação de igualdade
 def CPIG():
-    return
+    global D, s
+    if D[s-1] == D[s]:
+        D[s-1] = 1
+    else:
+        D[s-1] = 0
+    s = s-1
+    D.pop()
 
 #comparação de desigualdade
 def CDES():
-    return
+    global D, s
+    if D[s-1] != D[s]:
+        D[s-1] = 1
+    else:
+        D[s-1] = 0
+    s = s-1
+    D.pop()
 
 #comparação menor-igual
 def CPMI():
-    return
+    global D, s
+    if D[s-1] <= D[s]:
+        D[s-1] = 1
+    else:
+        D[s-1] = 0
+    s = s-1
+    D.pop()
 
 #comparação maior-igual
 def CMAI():
-    return
+    global D, s
+    if D[s-1] >= D[s]:
+        D[s-1] = 1
+    else:
+        D[s-1] = 0
+    s = s-1
+    D.pop()
 
 #armazena o topo da pilha no endereço n de D
 def ARMZ(n):
-    return
+    global D, s
+    D[n] = D[s]
+    s = s-1
+    D.pop()
 
 #desvio incondicional para a instrução de endereço p
 def DSVI(p):
-    return
+    global i
+    i = p - 1
 
 #desvio condicional para a instrução de endereço p; o desvio será
 #executado caso a condição resultante seja falsa; o valor da
 #condição estará no topo
 def DSVF(p):
-    return
+    global i, s, D
+    if D[s] == 0:
+        i = p - 1
+    s = s-1
+    D.pop()
 
 #lê um dado de entrada para o topo da pilha
 def LEIT():
-    return
+    global s, D
+    s = s+1
+    D.append(input("Digite o valor da entrada: "))
+    if isInt(D[s]):
+        D[s] = int(D[s])
+    else:
+        D[s] = float(D[s])
 
 #imprime valor o valor do topo da pilha na saída
 def IMPR():
-    return
+    global s, D
+    print(D[s])
+    s = s-1
+    D.pop()
 
 #reserva m posições na pilha D; m depende do tipo da variável
 def ALME(m):
-    return
+    global s, D
+    s = s + m
+    D.append(0)
 
 #inicia programa – será sempre a 1ª instrução
 def INPP():
-    return
+    global s
+    s = -1
 
 #termina a execução do programa
 def PARA():
-    return
+    exit()
 
 #aloca memória e copia valor da posição n para o topo de D
 def PARAM(n):
-    return
+    global s, D
+    s = s+1
+    D.append(0)
+    D[s] = D[n]
 
 #empilha o índice e da instrução seguinte à chamada do
 #procedimento, como endereço de retorno, no array C
 def PUSHER(e):
-    return
+    global s, D
+    s = s + 1
+    D.append(0)
+    D[s] = e
 
 #desvia para instrução de índice p no array C, obtido na TS
 def CHPR(p):
-    return
+    global i
+    i = p - 1
 
 #desaloca m posições de memória, a partir do topo s de D
 def DESM(m):
-    return
+    global s, D
+    s = s - m
+    for j in range(m):
+        D.pop()
 
 #retorna do procedimento – endereço de retorno estará no topo
 #de D – e desempilha o endereço
 def RTPR():
-    return
+    global i, s, D
+    i = D[s]
+    i = i - 1
+    s = s-1
+    D.pop()
 
-for instrucao in arquivo:
-    nLinha += 1
-    instrucao = instrucao.replace("\n","")
-    instrucao = instrucao.split(" ")
-
-    if len(instrucao) == 1:
-        comando = instrucao[0]
-        print(nLinha, comando)
+#verifica se é identificador
+def isIdent(ch):
+    if (ch[0] >= "a" and ch[0] <= "z") or (ch[0] >= "A" and ch[0] <= "Z"):
+        return True
     else:
-        comando = instrucao[0]
-        valor = instrucao[1]
-        print(nLinha, comando, valor)
+        return False
+
+#verifica se é inteiro
+def isInt(ch):
+    try: 
+        int(ch)
+        return True
+    except ValueError:
+        return False
+
+#verifica se é real
+def isReal(ch):
+    try: 
+        float(ch)
+        return True
+    except ValueError:
+        return False
+
+#Trata arquivo de entrada para variavel C
+arquivo = open("codigo.txt", "r")
+arquivo = arquivo.readlines()
+for j in range(len(arquivo)):
+    instrucao = arquivo[j].replace("\n","")
+    instrucao = instrucao.split(" ")
+    C.append(instrucao)
+
+#for i in range(len(C)):
+while i < len(C):
+
+    comando = C[i][0]
+    if len(C[i]) != 1:
+        comando = C[i][0]
+        if (isInt(C[i][1])):
+            valor = int(C[i][1])
+        else:
+            valor = float(C[i][1])
 
     if comando == "CRCT":
         CRCT(valor)
@@ -152,12 +262,6 @@ for instrucao in arquivo:
         DIVI()
     elif comando == "INVE":
         INVE()
-    elif comando == "CONJ":
-        CONJ()
-    elif comando == "DISJ":
-        DISJ()
-    elif comando == "NEGA":
-        NEGA()
     elif comando == "CPME":
         CPME()
     elif comando == "CPMA":
@@ -199,3 +303,5 @@ for instrucao in arquivo:
     else:
         print("Erro, instrucao %s desconhecida." %(comando))
         exit()
+
+    i += 1
